77109aca7ca46ac01b6606f46add25e92e9d660e
thequeenbea <turtlezdude@gmail.com>
2024-08-27 21:36:10 -0500

    replace mod txt with more comprehensive gd file

---
+++ _mods/info.gd
@@ -0,0 +1,9 @@
+## THIS SCRIPT IS NOT MEANT TO BE USED, AND ONLY EXISTS SO THE MOD FOLDER IS INCLUDED IN THE EXPORT
+# anyways make sure you keep all your mods in this folder!
+# when you're making a mod, make a folder for it in here, and keep all your changes related to the mod in there
+# contributions to the tool are very welcome, but make sure you don't structure them around your mod
+# (by this i mean make sure they work for general cases, not specifically your mod)
+# (and if you need to change files outside the mods folder then copy them into your mod and change them there)
+# anyways have fun! ask me for help if you need it :3
+
+var silly = true
