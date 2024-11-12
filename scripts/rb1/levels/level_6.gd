9a3ca68fbd02da482746fe511c4a2c36d645c53a
beatrixwashere <127708031+beatrixwashere@users.noreply.github.com>
2024-11-11 21:02:33 -0600

    start implementing bias into level 6

--- scripts/rb1/levels/level_6.gd
+++ scripts/rb1/levels/level_6.gd
@@ -1,30 +1,18 @@
 extends Node
 ## handles level 6 specific mechanics
 
-var drop_1_active: bool = false
-var drop_2_active: bool = false
-var drop_3_active: bool = false
-
 
 func _physics_process(_delta: float) -> void:
 	# rotate spinners
-	get_node("../../objects/spin").rotation += PI * 0.3 / 31
-	get_node("../../objects/spin/body").rotation += 0
+	#get_node("../../objects/spin").rotation += PI * 0.3 / 31
+	get_node("../../objects/spin").angular_velocity = PI * 0.3 / 31
 	get_node("../../objects/back_ball_1").rotation -= PI * 0.2 / 31
-	get_node("../../objects/back_ball_1/body").rotation -= 0
 	get_node("../../objects/back_ball_2").rotation -= PI * 0.2 / 31
-	get_node("../../objects/back_ball_2/body").rotation -= 0
 	get_node("../../objects/back_ball_3").rotation -= PI * 0.2 / 31
-	get_node("../../objects/back_ball_3/body").rotation -= 0
 	
 	# check for drop contact
-	if get_node("../..").redball.contact_monitor:
-		for i in get_node("../..").redball.get_colliding_bodies():
-			if i.get_parent().name == "drop_1":
-				drop_1_active = true
-			if i.get_parent().name == "drop_2":
-				drop_2_active = true
-			if i.get_parent().name == "drop_3":
-				drop_3_active = true
-	
-	# 
+	if get_node("../..")._is_alive:
+		for i in get_node("../..").contact_list:
+			if "drop" in i.get_parent().name:
+				get_node("../../objects/" + get_parent().name).density = 1
+				get_node("../../objects/" + get_parent().name).wake_up()
